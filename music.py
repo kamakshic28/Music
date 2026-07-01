0da4e2532bd8dcb6e219ceb80a2027a58b2ecf28
Kamakshi Chauhan <98561301+kamakshic28@users.noreply.github.com>
2026-07-01 01:32:32 -0400

    Add files via upload

--- music.py
+++ music.py
@@ -3,3 +3,8 @@ print("Music is listened by billions of people in this world but what makes it s
 print("1. Connection. I believe music often builds a connection from a person's mind to the mood or vibe the song is giving.")
 print("2. Nostalgia. I highly believe that many different types of music makes a person instantly remember their past.")
 print("3. Therapy. Music tends to heal a part of a person from within. Like they say, Music is therapy.")
+
+print("Intersting Music Facts")
+print("music can help reduce anxiety")
+print("music helps plants grow faster")
+print("a type of therapy known as music therapy helps heal brain damage and injuries")
